a82ebc7e42dcc2862f46356c7486196121c32716
GloriaCOCO <gloria.stvol@gmail.com>
2024-09-18 15:05:52 +0200

    finished concordance

---
+++ .ipynb_checkpoints/setup-checkpoint.sh
@@ -0,0 +1,8 @@
+# any python packages you want to have preinstalled
+#pip install pingouin
+#pip install lxml
+
+#putting in my credentials
+
+git config —global user.email "gloria.stvol@gmail.com"
+git config —global user.name "GloriaCOCO"
